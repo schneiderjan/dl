9a3e686616a939b77ff9dfc826040761f807afd9
Jan-Niklas Schneider <heisse.cola@gmail.com>
2019-05-15 12:02:45 +0200

    It is fucking working.

--- plotting.py
+++ plotting.py
@@ -1,24 +1,28 @@
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
+import pandas as pd
 from keras.engine.saving import load_model
 
-model = load_model('resnet50.hdf5')
+# model = load_model('models\\resnet50.hdf5')
+model_history = pd.read_csv('logs\\training_resnet50.log', sep=',', engine='python')
 
-print(history.history.keys())
+print(model_history)
+
+# print(history.history.keys())
 # summarize history for accuracy
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
+plt.plot(model_history['acc'])
+# plt.plot(model_history['val_acc'])
 plt.title('model accuracy')
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
 plt.show()
 # summarize history for loss
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
-plt.savefig('plots\\test.png')
+# plt.plot(model_history['loss'])
+# # plt.plot(model_history['val_loss'])
+# plt.title('model loss')
+# plt.ylabel('loss')
+# plt.xlabel('epoch')
+# plt.legend(['train', 'test'], loc='upper left')
+# plt.show()
+# plt.savefig('plots\\test.png')
--- run_resnet.py
+++ run_resnet.py
@@ -17,14 +17,13 @@ nr_samples = data.shape[0]
 nr_channels = 3
 nr_pixels = 224
 image_shape = (nr_pixels, nr_pixels, nr_channels)
-
 input_shape = Input(image_shape)
 
 # train test banana split
 print('Create train test split')
 X = np.array(data['image'].tolist())
 y = np.array(data['label'].tolist())
-X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, train_size=10)
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2)
 Y_train = np_utils.to_categorical(Y_train, nr_labels)
 Y_test = np_utils.to_categorical(Y_test, nr_labels)
 
@@ -34,7 +33,7 @@ nr_epochs = 40
 
 model_50 = keras_resnet.models.ResNet50(input_shape, classes=nr_labels)
 model_50.compile("adam", "categorical_crossentropy", ["accuracy"])
-csv_logger = CSVLogger('logs\\training_resnet50.log', separator=',', append=False)
+csv_logger = CSVLogger('logs\\resnet50.log', separator=',', append=False)
 history = model_50.fit(X_train, Y_train, verbose=1, callbacks=[csv_logger], epochs=nr_epochs)
 score = model_50.evaluate(X_test, Y_test, verbose=0)
 print('ResNet50 score:')
