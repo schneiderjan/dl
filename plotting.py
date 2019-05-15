dec3b0420024eedf9b076e4bd3ece2f03b879071
Jan-Niklas Schneider <heisse.cola@gmail.com>
2019-05-15 20:22:41 +0200

    Added plotting

--- plotting.py
+++ plotting.py
@@ -1,28 +1,47 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-from keras.engine.saving import load_model
+import numpy as np
 
-# model = load_model('models\\resnet50.hdf5')
-model_history = pd.read_csv('logs\\training_resnet50.log', sep=',', engine='python')
+model_names = ['ResNet18', 'ResNet34', 'ResNet50']#, 'ResNet101', 'ResNet152', 'ResNet200']
 
-print(model_history)
+# plot acc
+for name in model_names:
+    log_file = 'logs\\{}.log'.format(name)
+    model_history = pd.read_csv(log_file, sep=',')
 
-# print(history.history.keys())
-# summarize history for accuracy
-plt.plot(model_history['acc'])
-# plt.plot(model_history['val_acc'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
-# summarize history for loss
-# plt.plot(model_history['loss'])
-# # plt.plot(model_history['val_loss'])
-# plt.title('model loss')
-# plt.ylabel('loss')
-# plt.xlabel('epoch')
-# plt.legend(['train', 'test'], loc='upper left')
-# plt.show()
-# plt.savefig('plots\\test.png')
+    plt.plot(model_history['acc'])
+    # plt.plot(model_history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(model_names, loc='upper left')
+    plt.savefig('plots\\acc_{}.png'.format(name))
+
+plt.clf()
+
+# plot loss
+for name in model_names:
+    log_file = 'logs\\{}.log'.format(name)
+    model_history = pd.read_csv(log_file, sep=',')
+
+    plt.plot(model_history['loss'])
+    # plt.plot(model_history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(model_names, loc='upper left')
+    plt.savefig('plots\\loss_{}.png'.format(name))
+
+plt.clf()
+
+# bat plot acc
+x = np.arange(len(model_names))
+accuracies = []
+for name in model_names:
+    log_file = 'logs\\{}.log'.format(name)
+    model_history = pd.read_csv(log_file, sep=',')
+    accuracies.append(model_history.at[39, 'acc'])
+fig, ax = plt.subplots()
+plt.bar(x, accuracies)
+plt.xticks(x, model_names)
+plt.savefig('plots\\bar_acc.png')
